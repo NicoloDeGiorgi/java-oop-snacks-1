be3ec452de66a6700b254dfc19640ad6ee490b3e
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-06-15 12:03:22 +0200

    add : sanck 2

--- src/main/java/org/snack/studente/GestioneConto.java
+++ src/main/java/org/snack/studente/GestioneConto.java
@@ -1,5 +1,38 @@
 package org.snack.studente;
 
+//Snack 2: Gestione di un Conto
+//Crea una classe ContoBancario con attributi per numero di conto e saldo. 
+//Implementa un costruttore che accetta il numero di conto e inizializza il saldo a zero. 
+//Aggiungi metodi pubblici per depositare denaro sul conto, prelevare denaro dal conto e ottenere il saldo corrente.
+
 public class GestioneConto {
+	private int numeroConto;
+	private int saldo;
+	
+	public GestioneConto(int numeroConto) {
+		this.numeroConto = numeroConto;
+		this.saldo = 0;
+	}
+	
+	public int getSaldo() {
+		return this.saldo;
+	}
+	
+	public void deposito(int numeroDeposito){
+		this.saldo += numeroDeposito;
+	}
+	public void prelievo(int numeroPrelievo) {
+		int saldoPossibile = this.saldo - numeroPrelievo;
+		if (saldoPossibile < 0) {
+			this.saldo = saldoPossibile;
+		}
+	}
+	
+	
+	
+	public static void main(String[] args) {
+	
+	}
 
 }
+
