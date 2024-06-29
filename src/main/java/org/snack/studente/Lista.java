57153abff80add1db27bdc7b47ce853e926eb45c
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-06-29 10:18:26 +0200

    add: Array List

---
+++ src/main/java/org/snack/studente/Lista.java
@@ -0,0 +1,27 @@
+package org.snack.studente;
+import java.util.ArrayList;
+//Snack 3: Lista 
+//Creare un ArrayList di 10 nomi (stringhe) in maniera non ordinata
+//A - Ordinare l'array manualmente in ordine alfabetico (cioe' senza l'ausilio di un metodo)
+//B - Ordinarlo con l'ausilio di un metodo apposito
+
+public class Lista {
+	
+	public static void main(String[] args) {
+		
+ArrayList<String> lista = new ArrayList<>();
+		
+		lista.add("Federico");
+		lista.add("Lucia");
+		lista.add("Tony");
+		lista.add("Albeto");
+		lista.add("Patric");
+		lista.add("Giovanni");
+		lista.add("Vanessa");
+		lista.add("Sara");
+		lista.add("Chiara");
+		
+		
+	}
+
+}
